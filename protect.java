b1bf667cf7498589259e38f0535c8efa3158af0f
Taybur <you@example.com>
2021-10-16 00:41:53 -0400

    Hi

---
+++ protect.java
@@ -0,0 +1,17 @@
+package Javapractice3;//different package
+
+import JavaPractice.herit;//imported from jre
+
+public class protect extends herit{//protect class is extending parent class
+
+	
+	public static void main(String[] args) {//main method
+		
+		protect pro = new protect();//object of protect class
+		pro.dadu();//call method
+		
+		
+	}
+
+}
+
